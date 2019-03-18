32e1df876a00a0564aeb04f32cfe84e7288fe69e
Joao Hencke <joao.hencke@gmail.com>
2019-03-17 21:06:55 -0300

    removed dependencies

--- pages/support.jsx
+++ pages/support.jsx
@@ -1,15 +1,8 @@
 import React, { Component } from 'react';
-import Head from '../components/head';
-import Nav from '../components/nav';
 
 class Support extends Component {
   render() {
-    return (
-      <div className="container">
-        <Head />
-        <Nav />
-      </div>
-    );
+    return <div className="container" />;
   }
 }
 
