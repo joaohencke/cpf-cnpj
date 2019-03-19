2778f8ddbe0f2865bcea92fc2c93c2b22c74dcff
Joao Hencke <joao.hencke@gmail.com>
2019-03-19 15:03:36 -0300

    added support page. Renamed to be in pt-br (route)

--- routes.js
+++ routes.js
@@ -3,4 +3,5 @@ const routes = require('next-routes');
 module.exports = routes()
   .add('novo', '/novo')
   .add('index', '/')
-  .add('editar', '/editar/:_id');
+  .add('editar', '/editar/:_id')
+  .add('suporte', '/suporte');
